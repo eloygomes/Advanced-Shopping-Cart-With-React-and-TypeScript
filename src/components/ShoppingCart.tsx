c6c355198c801f8570936cb369539472266199ab
Eloy Gomes <eloy.gomes@icloud.com>
2023-11-28 06:27:13 -0300

    0.7.0.0
    - Finished

--- src/components/ShoppingCart.tsx
+++ src/components/ShoppingCart.tsx
@@ -1,20 +1,25 @@
-import { Offcanvas } from "react-bootstrap";
+import { Offcanvas, Stack } from "react-bootstrap";
 import { useShoppingCart } from "../context/ShoppingCartContext";
+import { CartItem } from "./CartItem";
 
 type ShoppingCartProps = {
   isOpen: boolean;
 };
 
 export function ShoppingCart({ isOpen }: ShoppingCartProps) {
-  const { closeCart } = useShoppingCart();
+  const { closeCart, cartItems } = useShoppingCart();
   return (
     <Offcanvas show={isOpen} placement="end" onHide={closeCart}>
       <Offcanvas.Header closeButton>
         <Offcanvas.Title>Cart</Offcanvas.Title>
       </Offcanvas.Header>
-      {/* <Offcanvas.Body>
-        <p>Shopping Cart</p>
-      </Offcanvas.Body> */}
+      <Offcanvas.Body>
+        <Stack gap={3}>
+          {cartItems.map((item) => (
+            <CartItem key={item.id} {...item} />
+          ))}
+        </Stack>
+      </Offcanvas.Body>
     </Offcanvas>
   );
 }
